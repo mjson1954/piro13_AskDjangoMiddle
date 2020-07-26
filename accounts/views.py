851ea5771da9e7cb774550789266f818cd351b7d
mjson1954 <mjson1954@gmail.com>
2020-07-27 01:51:05 +0900

    3강

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def profile(reqeust):
+    return render(reqeust, 'accounts/profile.html')
