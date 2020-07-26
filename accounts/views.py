f42cdaebd14147805524da067bb349dfaa28a904
mjson1954 <mjson1954@gmail.com>
2020-07-27 02:52:50 +0900

    8강

--- accounts/views.py
+++ accounts/views.py
@@ -13,19 +13,23 @@ from .forms import SignupForm
 #         if form.is_valid():
 #             user = form.save()
 #             auth_login(request, user)
+#             next_url = request.GET.get('next') or 'profile'
 #             return redirect('profile')
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html', {
 #         'form': form,
 #     })
+
 class SignupView(CreateView):
     model = User
     form_class = SignupForm
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
+
     def form_valid(self, form):
         user = form.save()
         auth_login(self.request, user)
